5d2755e4909d9e89f527e45b2298dfc66eacea7f
Sandr4 <sandrapaolamb14@gmail.com>
2026-09-22 08:02:26 -0500

    puerto vite

--- frontend-react/vite.config.js
+++ frontend-react/vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
     allowedHosts: ['.trycloudflare.com'],
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'http://localhost:5280',
         changeOrigin: true,
       },
     },
